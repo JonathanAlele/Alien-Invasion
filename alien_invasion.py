a02992b79f117cca98c3938cebb5e94f9998ac6b
JonathanAlele <jonathanalele@gmail.com>
2024-08-04 22:15:49 +0200

    Exit game
    Added functionality to quit the game on a key press

--- alien_invasion.py
+++ alien_invasion.py
@@ -43,6 +43,8 @@ class AlienInvasion:
         if event.key == pygame.K_LEFT:
             #move the ship to the left
             self.ship.moving_left = True
+        if event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self,event):
         """Responds to keyup events"""
