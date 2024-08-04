f9f46634a6ff33b3db0644da5e8c676bc69def33
JonathanAlele <jonathanalele@gmail.com>
2024-08-04 21:33:22 +0200

    Refactoring
    Refactored code for checking for events and updating the main surface

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,15 +19,23 @@ class AlienInvasion:
         """Start the main loop for the game"""
         while True:
             #watching for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.fill(self.settings.background_colour)
-            self.ship.blitme()
-            
+            self._check_events()
+            self._update_screen()
             #make the most recently drawn screen visible
-            pygame.display.flip()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Update images on teh screen and flip to the new screen"""
+        self.screen.fill(self.settings.background_colour)
+        self.ship.blitme()
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     #make a game instance and run the game
